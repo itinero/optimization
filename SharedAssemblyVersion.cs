bbab9eafaea419204134367bec16691c1c0ad64d
ben.abelshausen <ben@osmsharp.com>
2016-11-25 13:41:29 +0100

    Fixed nuspec and add proper testrunner config.

--- SharedAssemblyVersion.cs
+++ SharedAssemblyVersion.cs
@@ -18,5 +18,5 @@
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("0.7.0")]
-[assembly: AssemblyInformationalVersion("0.7.0-alpha")]
+[assembly: AssemblyVersion("0.7.1")]
+[assembly: AssemblyInformationalVersion("0.7.1-alpha")]
