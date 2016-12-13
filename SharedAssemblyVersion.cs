a8aed62b8aea4eebaa25b7e23cc28635e12909de
ben.abelshausen <ben@osmsharp.com>
2016-12-13 11:36:35 +0100

    Updated nuspec with project url, license and icon.

--- SharedAssemblyVersion.cs
+++ SharedAssemblyVersion.cs
@@ -18,5 +18,5 @@
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("0.8.2")]
-[assembly: AssemblyInformationalVersion("0.8.2-alpha")]
+[assembly: AssemblyVersion("0.8.3")]
+[assembly: AssemblyInformationalVersion("0.8.3-alpha")]
