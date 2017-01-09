b976f980f1da7550ae0540b48c678ae172ac6e35
ben.abelshausen <ben@osmsharp.com>
2017-01-09 11:58:03 +0100

    Updated Itinero and nuspec.

--- SharedAssemblyVersion.cs
+++ SharedAssemblyVersion.cs
@@ -18,5 +18,5 @@
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("0.8.13")]
-[assembly: AssemblyInformationalVersion("0.8.13-alpha")]
+[assembly: AssemblyVersion("0.8.14")]
+[assembly: AssemblyInformationalVersion("0.8.14-alpha")]
