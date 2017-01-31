f600614e8e4cf8e00b0432d47c4fd23a174d8a25
ben.abelshausen <ben@osmsharp.com>
2017-01-31 10:32:30 +0100

    Updated Itinero.

--- SharedAssemblyVersion.cs
+++ SharedAssemblyVersion.cs
@@ -18,5 +18,5 @@
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("0.12.1")]
-[assembly: AssemblyInformationalVersion("0.12.1-beta")]
+[assembly: AssemblyVersion("0.12.2")]
+[assembly: AssemblyInformationalVersion("0.12.2-beta")]
--- test/Itinero.Optimization.Test.Functional/Program.cs
+++ test/Itinero.Optimization.Test.Functional/Program.cs
@@ -43,41 +43,41 @@ namespace Itinero.Optimization.Test.Functional
             var router = new Router(routerDb);
 
             // define test locations.
-            // ANTWERPEN-NOORD
-            var locations = new Coordinate[]
-            {
-                new Coordinate(51.218644911468550f, 4.433176517486572f),
-                new Coordinate(51.217878824149580f, 4.433691501617432f),
-                new Coordinate(51.219142189396116f, 4.432237744331360f),
-                new Coordinate(51.219955294434854f, 4.430692791938782f),
-                new Coordinate(51.220788544202456f, 4.428203701972961f),
-                new Coordinate(51.219904895779710f, 4.428101778030395f),
-                new Coordinate(51.219558823524130f, 4.427565336227417f),
-                new Coordinate(51.218577711336620f, 4.428439736366271f),
-                new Coordinate(51.218234989137960f, 4.429646730422974f),
-                new Coordinate(51.219431145724300f, 4.434539079666138f),
-                new Coordinate(51.218786031426326f, 4.426513910293579f),
-                new Coordinate(51.221211883064340f, 4.429571628570557f)
-            };
-            //// WECHELDERZANDE
+            //// ANTWERPEN-NOORD
             //var locations = new Coordinate[]
             //{
-            //    new Coordinate(51.270453873703080f, 4.8008108139038080f),
-            //    new Coordinate(51.264197451065370f, 4.8017120361328125f),
-            //    new Coordinate(51.267446600889850f, 4.7830009460449220f),
-            //    new Coordinate(51.260733228426076f, 4.7796106338500980f),
-            //    new Coordinate(51.256489871317920f, 4.7884941101074220f),
-            //    new Coordinate(51.270964016530680f, 4.7894811630249020f),
-            //    new Coordinate(51.26216325894976f, 4.779932498931885f),
-            //    new Coordinate(51.26579184564325f, 4.777781367301941f),
-            //    new Coordinate(51.26855085674035f, 4.779181480407715f),
-            //    new Coordinate(51.26906437701784f, 4.7879791259765625f),
-            //    new Coordinate(51.259820134021695f, 4.7985148429870605f),
-            //    new Coordinate(51.257040455587656f, 4.780147075653076f),
-            //    new Coordinate(51.256248149311716f, 4.788386821746826f),
-            //    new Coordinate(51.270054481615624f, 4.799646735191345f),
-            //    new Coordinate(51.252984777835955f, 4.776681661605835f)
+            //    new Coordinate(51.218644911468550f, 4.433176517486572f),
+            //    new Coordinate(51.217878824149580f, 4.433691501617432f),
+            //    new Coordinate(51.219142189396116f, 4.432237744331360f),
+            //    new Coordinate(51.219955294434854f, 4.430692791938782f),
+            //    new Coordinate(51.220788544202456f, 4.428203701972961f),
+            //    new Coordinate(51.219904895779710f, 4.428101778030395f),
+            //    new Coordinate(51.219558823524130f, 4.427565336227417f),
+            //    new Coordinate(51.218577711336620f, 4.428439736366271f),
+            //    new Coordinate(51.218234989137960f, 4.429646730422974f),
+            //    new Coordinate(51.219431145724300f, 4.434539079666138f),
+            //    new Coordinate(51.218786031426326f, 4.426513910293579f),
+            //    new Coordinate(51.221211883064340f, 4.429571628570557f)
             //};
+            // WECHELDERZANDE
+            var locations = new Coordinate[]
+            {
+                new Coordinate(51.270453873703080f, 4.8008108139038080f),
+                new Coordinate(51.264197451065370f, 4.8017120361328125f),
+                new Coordinate(51.267446600889850f, 4.7830009460449220f),
+                new Coordinate(51.260733228426076f, 4.7796106338500980f),
+                new Coordinate(51.256489871317920f, 4.7884941101074220f),
+                new Coordinate(51.270964016530680f, 4.7894811630249020f),
+                new Coordinate(51.26216325894976f, 4.779932498931885f),
+                new Coordinate(51.26579184564325f, 4.777781367301941f),
+                new Coordinate(51.26855085674035f, 4.779181480407715f),
+                new Coordinate(51.26906437701784f, 4.7879791259765625f),
+                new Coordinate(51.259820134021695f, 4.7985148429870605f),
+                new Coordinate(51.257040455587656f, 4.780147075653076f),
+                new Coordinate(51.256248149311716f, 4.788386821746826f),
+                new Coordinate(51.270054481615624f, 4.799646735191345f),
+                new Coordinate(51.252984777835955f, 4.776681661605835f)
+            };
 
             // calculate TSP.
             var func = new Func<Route>(() => router.CalculateTSP(Itinero.Osm.Vehicles.Vehicle.Car.Fastest(), locations, 0, 0));
