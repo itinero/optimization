3abce6339cf59846d1c89390f2fd33c32a7b155b
xivk <ben@itinero.tech>
2018-01-31 12:09:18 +0000

    Added new CI helpers.

--- src/Itinero.Optimization/Algorithms/CheapestInsertion/CheapestInsertionHelper.cs
+++ src/Itinero.Optimization/Algorithms/CheapestInsertion/CheapestInsertionHelper.cs
@@ -16,6 +16,7 @@
  *  limitations under the License.
  */
 
+using System.Collections.Generic;
 using Itinero.Optimization.Tours;
 
 namespace Itinero.Optimization.Algorithms.CheapestInsertion
@@ -86,6 +87,56 @@ namespace Itinero.Optimization.Algorithms.CheapestInsertion
         }
 
         /// <summary>
+        /// Calculates the best position to insert any of the given visits.
+        /// </summary>
+        /// <param name="tour">The tour to insert into.</param>
+        /// <param name="weights">The weights between visits.</param>
+        /// <param name="visits">The visits to potentially insert.</param>
+        /// <param name="location">The cheapest location to insert.</param>
+        /// <param name="visit">The cheapest visit to insert.</param>
+        /// <returns>The increase/decrease in weight.</returns>
+        public static float CalculateCheapestAny(this ITour tour, float[][] weights, IEnumerable<int> visits, 
+            out Pair location, out int visit)
+        {
+            // TODO: test performance if we turn this loop around, now it's tour and inner is target visits.
+            var bestCost = float.MaxValue;
+            location = new Pair(Constants.NOT_SET, Constants.NOT_SET);
+            visit = Constants.NOT_SET;
+
+            if (tour.Count == 1)
+            {
+                var first = tour.First;
+                if (tour.IsClosed())
+                {
+                    visit = weights.CalculateCheapest(first, first, visits, out bestCost);
+                    location = new Pair(first, first);
+                }
+                else
+                {
+                    visit = weights.CalculateCheapestTo(first, visits, out bestCost);
+                    location = new Pair(first, int.MaxValue);
+                }
+            }
+            else
+            {
+                foreach (var pair in tour.Pairs())
+                {
+                    float cost;
+                    var localVisit = weights.CalculateCheapest(pair.From, pair.To, visits, out cost);
+                    cost -= weights[pair.From][pair.To];
+                    if (cost < bestCost)
+                    {
+                        bestCost = cost;
+                        visit = localVisit;
+                        location = pair;
+                    }
+                }
+            }
+
+            return bestCost;
+        }
+
+        /// <summary>
         /// Calculates the cheapest location to insert a sub-path that consists of (from) -> ... -> (to).
         /// </summary>
         /// <param name="tour">The tour to insert into.</param>
@@ -129,6 +180,70 @@ namespace Itinero.Optimization.Algorithms.CheapestInsertion
                 }
             }
             return bestCost;
+        }
+
+        /// <summary>
+        /// Returns the cheapest target to reach from the given source visit.
+        /// </summary>
+        /// <param name="weights">The weights between all visits.</param>
+        /// <param name="source">The source visit.</param>
+        /// <param name="targets">The target visits.</param>
+        /// <param name="weight">The weight to the found target.</param>
+        /// <returns>The cheapest target to reach from the given source visit.</returns>
+        public static int CalculateCheapestTo(this float[][] weights, int source, IEnumerable<int> targets,
+            out float weight)
+        {
+            return weights.CalculateCheapest(source, Constants.NOT_SET, targets, out weight);
+        }
+
+        /// <summary>
+        /// Returns the cheapest source visit to reach the given target.
+        /// </summary>
+        /// <param name="weights">The weights between all visits.</param>
+        /// <param name="target">The target visit.</param>
+        /// <param name="sources">The source visits.</param>
+        /// <param name="weight">The weight to the found target.</param>
+        /// <returns>The cheapest target to reach from the given source visit.</returns>
+        public static int CalculateCheapestFrom(this float[][] weights, int target, IEnumerable<int> sources,
+            out float weight)
+        {
+            return weights.CalculateCheapest(Constants.NOT_SET, target, sources, out weight);
+        }
+
+        /// <summary>
+        /// Returns the cheapest visit the complete the sequence source->visit->target from the visits given.
+        /// </summary>
+        /// <param name="weights">The weights between all visits.</param>
+        /// <param name="source">The source visit, ignored if Constants.NOT_SET.</param>
+        /// <param name="target">The target visit, ignored if Constants.NOT_SET.</param>
+        /// <param name="visits">The visits to consider.</param>
+        /// <param name="weight">The weight of the path of the cheapest visit, source->visit->target. The weight between source->target is NOT subtracted.</param>
+        /// <returns>The cheapest visit the complete the sequence source->visit->target from the visits given.</returns>
+        public static int CalculateCheapest(this float[][] weights, int source, int target, IEnumerable<int> visits,
+            out float weight)
+        {
+            weight = float.MaxValue;
+            var best = Constants.NOT_SET;
+
+            foreach (var visit in visits)
+            {
+                var localWeight = 0f;
+                if (source != Constants.NOT_SET)
+                {
+                    localWeight += weights[source][visit];
+                }
+                if (target != Constants.NOT_SET)
+                {
+                    localWeight += weights[visit][target];
+                }
+                if (localWeight < weight)
+                {
+                    weight = localWeight;
+                    best = target;
+                }
+            }
+
+            return best;
         }
     }
 }
