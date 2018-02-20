5bdcb7dc92b46517c95daff1c54f95cec2c5e255
xivk <ben@itinero.tech>
2018-02-20 14:37:43 +0000

    Removed CVRP extension methods, not supported just yet.

--- src/Itinero.Optimization/RouterExtensions.cs
+++ src/Itinero.Optimization/RouterExtensions.cs
@@ -187,38 +187,5 @@ namespace Itinero.Optimization
         {
             return router.TryCalculateSTSPDirected(profile, locations, turnPenaltyInSeconds, max, first, last).Value;
         }
-
-        /// <summary>
-        /// Calculates a no-depot CVRP.
-        /// </summary>
-        /// <param name="router">The router.</param>
-        /// <param name="profile">The profile to calculate for.</param>
-        /// <param name="locations">The locations.</param>
-        /// <param name="max">The maximum travel time per tour.</param>
-        /// <returns></returns>
-        public static Result<List<Route>> TryCalculateNoDepotCVRP(this Router router, Profile profile, Coordinate[] locations, float max)
-        {
-            var vrpRouter = new Itinero.Optimization.VRP.NoDepot.Capacitated.NoDepotCVRPRouter(new WeightMatrixAlgorithm(router, profile, locations),
-                max);
-            vrpRouter.Run();
-            if (!vrpRouter.HasSucceeded)
-            {
-                return new Result<List<Route>>(vrpRouter.ErrorMessage);
-            }
-            return new Result<List<Route>>(vrpRouter.WeightMatrix.BuildRoutes(vrpRouter.RawSolution));
-        }
-
-        /// <summary>
-        /// Calculates a no-depot CVRP.
-        /// </summary>
-        /// <param name="router">The router.</param>
-        /// <param name="profile">The profile to calculate for.</param>
-        /// <param name="locations">The locations.</param>
-        /// <param name="max">The maximum travel time per tour.</param>
-        /// <returns></returns>
-        public static List<Route> CalculateNoDepotCVRP(this Router router, Profile profile, Coordinate[] locations, float max)
-        {
-            return router.TryCalculateNoDepotCVRP(profile, locations, max).Value;
-        }
     }
 }
