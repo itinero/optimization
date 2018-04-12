52286e9ff5db3a4bab00c008ade08b4fbfd0465f
xivk <ben@itinero.tech>
2018-04-12 10:31:13 +0200

    Bumped version #.

--- SharedAssemblyVersion.cs
+++ SharedAssemblyVersion.cs
@@ -18,5 +18,5 @@
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("0.18.3")]
-[assembly: AssemblyInformationalVersion("0.18.3-beta")]
+[assembly: AssemblyVersion("0.19.0")]
+[assembly: AssemblyInformationalVersion("0.19.0-beta")]
