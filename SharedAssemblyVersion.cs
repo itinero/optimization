4cfb008ba03d768c97c118f9b81c26ec93128597
Ben Abelshausen <ben@osmsharp.com>
2018-04-17 10:33:19 +0100

    Bumped version # and enabled all functional tests.

--- SharedAssemblyVersion.cs
+++ SharedAssemblyVersion.cs
@@ -18,5 +18,5 @@
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("0.19.2")]
-[assembly: AssemblyInformationalVersion("0.19.2-beta")]
+[assembly: AssemblyVersion("0.20.0")]
+[assembly: AssemblyInformationalVersion("0.20.0-beta")]
