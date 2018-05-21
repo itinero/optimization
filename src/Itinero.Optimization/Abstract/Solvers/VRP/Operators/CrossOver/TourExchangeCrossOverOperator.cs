16837d5176e273d5aa1d8a6e7e48a907dc6443a3
xivk <ben@itinero.tech>
2018-05-21 13:37:46 +0100

    Start of a genetic algorithm implementation for the VRP variants.

---
+++ src/Itinero.Optimization/Abstract/Solvers/VRP/Operators/CrossOver/TourExchangeCrossOverOperator.cs
@@ -0,0 +1,63 @@
+﻿/*
+ *  Licensed to SharpSoftware under one or more contributor
+ *  license agreements. See the NOTICE file distributed with this work for 
+ *  additional information regarding copyright ownership.
+ * 
+ *  SharpSoftware licenses this file to you under the Apache License, 
+ *  Version 2.0 (the "License"); you may not use this file except in 
+ *  compliance with the License. You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+using Itinero.Attributes;
+using Itinero.Optimization.Abstract.Solvers.VRP.Solvers.GA;
+using Itinero.Optimization.Algorithms.Solvers.GA;
+using Itinero.Optimization.Algorithms.Solvers.Objective;
+
+namespace Itinero.Optimization.Abstract.Solvers.VRP.Operators.CrossOver
+{
+    /// <summary>
+    /// A cross over operator that uses tours from the two given solution to build a new solution.
+    /// </summary>
+    /// <typeparam name="TObjective"></typeparam>
+    /// <typeparam name="TProblem"></typeparam>
+    /// <typeparam name="TSolution"></typeparam>
+    public class TourExchangeCrossOverOperator<TObjective, TProblem, TSolution> : ICrossOverOperator<float, TProblem, TObjective, TSolution, float>
+        where TObjective : ObjectiveBase<TProblem, TSolution, float>, IGAObjective<TProblem, TSolution>
+    {
+        /// <inheritdoc />
+        public string Name { get; } = "TOUR_EX";
+
+        /// <inheritdoc />
+        public TSolution Apply(TProblem problem, TObjective objective, TSolution solution1, TSolution solution2, out float fitness)
+        {
+            var solution = objective.NewSolution(problem);
+
+            // try to use as many tours as possible.
+            var success = true;
+            while (success)
+            {
+                success = false;
+                
+                // try to select a tour from solution1 to use in the given solution.
+                success |= objective.SelectTour(problem, solution1, solution);
+                
+                // try to select a tour from solution2 to use in the given solution.
+                success |= objective.SelectTour(problem, solution2, solution);
+            }
+            
+            // try to place the rest of the missing visits.
+            objective.PlaceRemaining(problem, solution);
+
+            fitness = objective.Calculate(problem, solution);
+            return solution;
+        }
+    }
+}
---
+++ src/Itinero.Optimization/Abstract/Solvers/VRP/Solvers/GA/IGAObjective.cs
@@ -0,0 +1,51 @@
+﻿/*
+ *  Licensed to SharpSoftware under one or more contributor
+ *  license agreements. See the NOTICE file distributed with this work for 
+ *  additional information regarding copyright ownership.
+ * 
+ *  SharpSoftware licenses this file to you under the Apache License, 
+ *  Version 2.0 (the "License"); you may not use this file except in 
+ *  compliance with the License. You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+namespace Itinero.Optimization.Abstract.Solvers.VRP.Solvers.GA
+{
+    /// <summary>
+    /// Abstract representation of an objective.
+    /// </summary>
+    /// <typeparam name="TProblem">The problem type.</typeparam>
+    /// <typeparam name="TSolution">The solution type.</typeparam>
+    public interface IGAObjective<in TProblem, TSolution>
+    {
+        /// <summary>
+        /// Creates a new and empty solution.
+        /// </summary>
+        /// <param name="problem">The problem.</param>
+        /// <returns>A new empty solution.</returns>
+        TSolution NewSolution(TProblem problem);
+
+        /// <summary>
+        /// Selects a good candidate tour to be place in the target tour from the source tour.
+        /// </summary>
+        /// <param name="problem">The problem.</param>
+        /// <param name="source">The source.</param>
+        /// <param name="target">The target.</param>
+        /// <returns>True if a good tour was found.</returns>
+        bool SelectTour(TProblem problem, TSolution source, TSolution target);
+
+        /// <summary>
+        /// Place all the remaining unplaced visits in the given solution.
+        /// </summary>
+        /// <param name="problem">The problem.</param>
+        /// <param name="solution">The solution.</param>
+        void PlaceRemaining(TProblem problem, TSolution solution);
+    }
+}
