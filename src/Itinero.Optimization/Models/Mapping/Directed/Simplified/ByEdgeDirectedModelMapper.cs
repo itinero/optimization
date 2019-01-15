3fb1843adc1feabab7c742710f57f7c93e8d9436
xivk <ben@itinero.tech>
2019-01-15 11:09:27 +0000

    Only do mapping of simple model once.

--- src/Itinero.Optimization/Models/Mapping/Directed/Simplified/ByEdgeDirectedModelMapper.cs
+++ src/Itinero.Optimization/Models/Mapping/Directed/Simplified/ByEdgeDirectedModelMapper.cs
@@ -1,6 +1,7 @@
 using System;
 using Itinero.Algorithms.Matrices;
 using Itinero.LocalGeo;
+using Itinero.Logging;
 using Itinero.Optimization.Models.Simplification.ByEdge;
 using Itinero.Optimization.Models.Visits.Costs;
 
@@ -31,12 +32,6 @@ namespace Itinero.Optimization.Models.Mapping.Directed.Simplified
             if (!EdgeBasedSimplifier.TrySimplify(router, model, out var simplified, out message))
             {
                 return DirectedModelMapper.TryMap(router, model, out mappings, out message);
-            }
-
-            if (!DirectedModelMapper.TryMap(router, simplified.model, out mappings, out message))
-            {
-                mappings = (null, null);
-                return false;
             }
 
             // use simplified model from now on.
@@ -109,6 +104,8 @@ namespace Itinero.Optimization.Models.Mapping.Directed.Simplified
                 };
             }
             
+            Logger.Log(nameof(EdgeBasedSimplifier), TraceEventType.Verbose,
+                $"Calculating directed matrix for {simplified.massResolvingAlgorithm.RouterPoints.Count} visits!");
             var weightMatrixAlgorithm = new DirectedWeightMatrixAlgorithm(router, profile, simplified.massResolvingAlgorithm);
             weightMatrixAlgorithm.Run();
             if (!weightMatrixAlgorithm.HasSucceeded)
@@ -118,6 +115,8 @@ namespace Itinero.Optimization.Models.Mapping.Directed.Simplified
                 mappings = (null, null);
                 return false;
             }
+            Logger.Log(nameof(EdgeBasedSimplifier), TraceEventType.Verbose,
+                $"Calculating directed matrix for {simplified.massResolvingAlgorithm.RouterPoints.Count} visits finished");
             
             // try to adjust the vehicle pool to the mapping, if any of the defined departure or arrival points cannot be mapped then this will fail.
             if (!weightMatrixAlgorithm.TryToMap(model.VehiclePool, out var mappedVehiclePool, out message))
